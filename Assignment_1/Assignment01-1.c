9d6991737f1bdfadcc1283bb1a639aaaf6a974fc
HarshadGangarde <gangardeharshad2005@gmail.com>
2025-09-27 21:33:44 +0530

    Add files via upload

---
+++ Assignment_1/Assignment01-1.c
@@ -0,0 +1,28 @@
+// Program to Divide two Numbers
+
+#include<stdio.h>
+
+int Division(int iNo1, int iNo2)
+{
+    int iAns = 0;
+    if(iNo2 > iNo1)
+    {
+        return -1;
+    }
+
+    iAns = iNo1 / iNo2;
+
+    return iAns;
+}
+
+int main()
+{
+    int iValue1 = 15, iValue2 = 5;
+    int iRet = 0;
+
+    iRet = Division(iValue1 , iValue2);
+
+    printf("Division is :%d\n",iRet);
+
+    return 0;
+}
