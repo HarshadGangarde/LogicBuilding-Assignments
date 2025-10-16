c9a19cf8b97bda1263151dc156a5351176d073e3
HarshadGangarde <gangardeharshad2005@gmail.com>
2025-10-16 22:18:29 +0530

    Add files via upload

---
+++ Assignment_11/Assignment11-1.c
@@ -0,0 +1,31 @@
+////////////////////////////////
+//  Input : 5
+//  Output : A  B  C  D  E
+///////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iNo)
+{
+    int iCnt = 0;
+    char ch = '\0';
+    ch = 'A';
+
+    for (iCnt = 1; iCnt <= iNo; iCnt++)
+    {
+        printf("%c\t",ch);
+        ch++;
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter Number : \n");
+    scanf("%d",&iValue);
+
+    Pattern(iValue);
+
+    return 0;
+}
