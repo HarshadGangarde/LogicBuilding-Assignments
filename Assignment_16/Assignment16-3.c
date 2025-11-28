c62f537178e3b7655981ee4673dc25f4e9412e64
HarshadGangarde <gangardeharshad2005@gmail.com>
2025-11-28 12:56:14 +0530

    Add files via upload

---
+++ Assignment_16/Assignment16-3.c
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////
+//  Input : 4     4
+//  Output : *  *  *  *
+//           *  *     *   
+//           *     *  *  
+//           *  *  *  *   
+////////////////////////////////////////////////
+
+#include<stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int iCnt = 0, jCnt = 0;
+
+    for (iCnt = 1; iCnt <= iRow; iCnt++)
+    {
+        for (jCnt = 1; jCnt <= iCol; jCnt++)
+        {
+
+            if ((iCnt == 1) || (jCnt == 1) ||(iCnt == iCol) || (jCnt == iRow) || (iCnt == jCnt))
+            {
+                printf("*\t");
+            }
+            else
+            {
+                printf("\t");
+            }
+            
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int iValue1 = 0, iValue2 = 0;
+
+    printf("Enter Number of Rows : \n");
+    scanf("%d",&iValue1);
+
+    printf("Enter Number of Columns : \n");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1, iValue2);
+
+    return 0;
+}
